d5757c144a0b1a8b3e060fc6c6129377a2974357
Gabriele <gabriele.nevini@gmail.com>
2022-04-22 09:55:00 +0200

    Mapa al fin

---
+++ ProyectoFinal/Mapa.aspx.cs
@@ -0,0 +1,25 @@
+﻿using ProyectoFinal.DALs;
+using ProyectoFinal.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProyectoFinal
+{
+    public partial class Mapa : System.Web.UI.Page
+    {
+        List<Residencia> listResis;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DALResidencia dalresi = new DALResidencia();
+            listResis = dalresi.SelectAll();
+            foreach (Residencia item in listResis)
+            {
+                
+            }
+        }
+    }
+}
